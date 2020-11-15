c39bc40b864a04bbbc1ea958cb4dc8811d793bde
FoxSGR <foxsgrno1@gmail.com>
2020-11-15 02:16:21 +0000

    load config before connecting to db

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,6 +2,8 @@ import { initMongoose } from './init-mongoose';
 import * as dotenv from 'dotenv';
 
 export default () => {
+  dotenv.config();
+
   console.log('Connecting to the database');
   initMongoose()
     .then(() => console.log('Connected to the database'))
@@ -9,6 +11,4 @@ export default () => {
       console.error(`Failed to connect to the database: ${e.message}`);
       console.error(e);
     });
-
-  dotenv.config();
 };
